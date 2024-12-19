c5b5132a6f662c19af78d07cbb1d72be06355be6
ellian.aragao <ellian.aragao@gmail.com>
2024-12-19 01:08:41 -0300

    feat: ultimo estado iniciando testes

--- 2024/projeto/1.asm
+++ 2024/projeto/1.asm
@@ -1,12 +1,18 @@
 .data
 vetor: .word 1, 2, 3, 4, 5, 6, 7, 8, 9
-new_line: .asciiz "\n"
 
 .text
 
+la $a0, vetor
+li $a1, 0
+li $a2, 10
+li $a3, 5
+jal busca_binaria
+j exit
+
 busca_binaria:
-  addi $sp, $zero, -4
-  sw $ra, $sp # endereço de retorno
+  addi $sp, $zero, -4  # cria elemento na pilha
+  sw $ra, ($sp)        # endereço de retorno
 
   # VERIFICA SE INDICES SÃO VÁLIDOS
   slt $t5, $a2, $a1 # offset final < offset inicial ? 1 : 0
@@ -14,12 +20,12 @@ busca_binaria:
 
   # CORPO FUNÇÃO DA BUSCA BINÁRIA
   srl $t0, $a2, 1       # índice do meio do vetor | offset final / 2
-  add $t1, $t0, $a0     # offset do endereço do elemento do meio do vetor
-  lw  $t3, $t1          # carrega valor do elemento do meio do vetor
+  sll $t1, $t0, 2       # índice do meio do vetor * 4 bytes
+  add $t1, $t1, $a0     # offset do endereço do elemento do meio do vetor
+  lw  $t3, ($t1)        # carrega valor do elemento do meio do vetor
   beq $a3, $t3, conclui # elemento do meio = elemento buscado => conclui
   slt $t4, $a3, $t3     # elemento do meio < valor procurado ? 1 : 0
   bne $t4, $zero, busca_binaria_direita # elemento do meio > valor procurado => busca no lado direito do vetor
-
 
   busca_binaria_esquerda:
     add $a2, $t0, $zero   # offset final = índice do meio do vetor
@@ -30,13 +36,15 @@ busca_binaria:
     jal busca_binaria     # executa busca binária recursiva
 
   conclui:
-    addi $v0, $t0, $zero # índice do elemento como retorno
+    add $v0, $t0, $zero   # índice do elemento como retorno
     j remove_pilha_e_retorna
 
   nao_encontrado:
     addi $v0, $zero, -1  # elemento não encontrado
 
   remove_pilha_e_retorna:
-    lw $ra, $sp       # carrega endereço de retorno da pilha
+    lw $ra, ($sp)     # carrega endereço de retorno da pilha
     addi $sp, $sp, 4  # remove item da pilha
     jr $ra            # retorna pro último procedimento chamado
+
+exit:
