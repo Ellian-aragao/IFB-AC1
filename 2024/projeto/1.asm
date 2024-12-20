a3827acc75d04bb7726bd769fef43964f722c36c
ellian.aragao <ellian.aragao@gmail.com>
2024-12-19 22:06:56 -0300

    feat: implementação testada com caso de busca a esquerda de elemento

--- 2024/projeto/1.asm
+++ 2024/projeto/1.asm
@@ -1,18 +1,18 @@
 .data
-vetor: .word 1, 2, 3, 4, 5, 6, 7, 8, 9
+vetor: .word 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 .text
 
 la $a0, vetor
 li $a1, 0
 li $a2, 10
-li $a3, 5
+li $a3, 1
 jal busca_binaria
 j exit
 
 busca_binaria:
-  addi $sp, $zero, -4  # cria elemento na pilha
-  sw $ra, ($sp)        # endereço de retorno
+  addi $sp, $sp, -4  # cria elemento na pilha
+  sw $ra, 0($sp)       # endereço de retorno
 
   # VERIFICA SE INDICES SÃO VÁLIDOS
   slt $t5, $a2, $a1 # offset final < offset inicial ? 1 : 0
@@ -24,16 +24,18 @@ busca_binaria:
   add $t1, $t1, $a0     # offset do endereço do elemento do meio do vetor
   lw  $t3, ($t1)        # carrega valor do elemento do meio do vetor
   beq $a3, $t3, conclui # elemento do meio = elemento buscado => conclui
-  slt $t4, $a3, $t3     # elemento do meio < valor procurado ? 1 : 0
+  slt $t4, $t3, $a3     # elemento do meio < valor procurado ? 1 : 0
   bne $t4, $zero, busca_binaria_direita # elemento do meio > valor procurado => busca no lado direito do vetor
 
   busca_binaria_esquerda:
     add $a2, $t0, $zero   # offset final = índice do meio do vetor
     jal busca_binaria     # executa busca binária recursiva
+    j remove_pilha_e_retorna
 
   busca_binaria_direita:
     add $a1, $t0, $zero   # offset inicial = índice do meio do vetor
     jal busca_binaria     # executa busca binária recursiva
+    j remove_pilha_e_retorna
 
   conclui:
     add $v0, $t0, $zero   # índice do elemento como retorno
@@ -43,7 +45,7 @@ busca_binaria:
     addi $v0, $zero, -1  # elemento não encontrado
 
   remove_pilha_e_retorna:
-    lw $ra, ($sp)     # carrega endereço de retorno da pilha
+    lw $ra, 0($sp)    # carrega endereço de retorno da pilha
     addi $sp, $sp, 4  # remove item da pilha
     jr $ra            # retorna pro último procedimento chamado
 
