2334d7170953a24be0898528bd9976b64cbff55f
Ellian Aragão Dias <ellian.aragao@gmail.com>
2024-12-27 00:16:11 -0300

    feat: implementação strcmp para validação do string ano

--- 2024/projeto/3.asm
+++ 2024/projeto/3.asm
@@ -1,9 +1,13 @@
 .data
   erro_le_arquivo_str: .asciiz "Erro ao ler arquivo"
   erro_abrir_arquivo:  .asciiz "Erro ao abrir arquivo"
-  nome_arquivo:        .asciiz "/Users/ellian/code/faculdade/IFB-AC1/2024/projeto/1.asm"
+  nome_arquivo:        .asciiz "/home/ellian/code/faculdade/IFB-AC1/2024/projeto/sample/t2.txt"
+
   buffer:              .space 1024  # buffer para armazenar o conteúdo do arquivo
-  size_buffer:         .word  1024  # tamanho do buffer
+  buffer_ano:          .space 40    # buffer para armazenar o ano do arquivo
+
+  size_ano:            .word 4  # quantidade de caracteres para ano
+  size_ignore:         .word 4  # quantidade de caracteres para ignorar informação
 
 .text
 
@@ -16,24 +20,24 @@ main:
     add $a0, $s0, $zero                      # configura descritor do arquivo como argumento
     la $a1, buffer                           # configura buffer de leitura
 
-    calcula_quantos_caracteres_o_buffer_suporta:
-      la $t0, size_buffer                    # carrega endereço tamanho buffer
+    calcula_quantos_caracteres_o_buffer_deve_ler:
+      la $t0, size_ano                       # carrega endereço tamanho das strings ano
       lw $t0, ($t0)                          # carrega valor do endereço do tamanho do buffer
-      srl $t2, $t0, 3                        # numero de caracteres =  tamanho buffer / 8
       add $a2, $t2, $zero                    # configura numero de caracteres para leitura
 
     le_arquivo_e_finaliza_se_eof:
       jal le_arquivo                           # chama leitura de arquivo
-      beq $v0, $zero, fecha_arquivo_e_finaliza # leitura = EOF => fecha arquivo e finaliza execução
+      beq $v0, $zero, fecha_arquivo_e_finaliza # leitura == EOF => fecha arquivo e finaliza execução
 
     verifica_erro_na_leitura_do_arquivo:
       slt $t0, $v0, $zero              # retorno leitura arquivo < 0 ? 1 : 0
       bne $t0, $zero, erro_le_arquivo  # retorno leitura arquivo < 0 => encerra processo com erro
 
     carrega_chamada_print_da_leitura_retorna_loop:
-      add $a0, $a1, $zero              # carrega o buffer para print da string
+      la $a0, buffer_ano               # configura buffer de escrita
       jal print_string                 # chama procedimento de print da string
-      j loop                           # volta no loop
+
+    j loop                           # volta no loop
 
   fecha_arquivo_e_finaliza:
     add $a0, $s0, $zero            # configura descritor como argumento do fechamento de arquivo
@@ -43,6 +47,24 @@ main:
 
 
 # Functions
+
+# https://stackoverflow.com/questions/53039818/understanding-the-strcmp-function-of-gnu-libc
+# int strcmp (const char *p1, const char *p2)
+strcmp:
+  while_strcmp:
+    lb $t0, 0($a0)       # carrega primeiro byte do argumento a0
+    lb $t1, 0($a1)       # carrega primeiro byte do argumento a1
+
+    beq $t0, $zero retorna_subtracao # break
+
+    incrementa_endereco_string:
+      addi $a0, $a0, 8   # a0 += 8 bytes => endereço próximo caractere
+      addi $a1, $a1, 8   # a1 += 8 bytes => endereço próximo caractere
+
+    beq $t0, $t1, while_strcmp
+
+  subu $v0, $t0, $t1  # v0 = t0 - t1
+  jr $ra              # retorna para ultima instrução a chamar a função
 
 abre_arquivo_leitura:
   addi $v0, $zero, 13   # código para abrir arquivo
